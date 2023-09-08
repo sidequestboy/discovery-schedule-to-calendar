d5cc5a19b48f66cba547c279ae8e17c781869895
Jamie Macdonald <jamie.alban@gmail.com>
2023-09-08 15:02:26 -0700

    Put developer api_key in .env instead

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,12 +1,15 @@
-// Not sensitive secrets
-// api_key is restricted by http referrer to my website
-// use a separate developer api_key for development.
-const authParams = {
+// public_api_key is restricted by http referrer to my website only.
+//
+// For development on localhost, I have a separate key stored in
+// .env as DEV_API_KEY. This is ignored in version control
+//
+
+const public_api_key = "AIzaSyAfpJqpoeUGg8TlpCSyNxQSxkKu71uZqnA";
+
+export const authParams = {
   client_id:
     "917800094388-7l1psptt380l0srblir0v7a7gi5t8472.apps.googleusercontent.com",
   scope:
     "https://www.googleapis.com/auth/calendar.app.created https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/drive.readonly",
-  api_key: "AIzaSyAfpJqpoeUGg8TlpCSyNxQSxkKu71uZqnA",
+  api_key: process.env.DEV_API_KEY ? process.env.DEV_API_KEY : public_api_key,
 };
-
-export { authParams };
